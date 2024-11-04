9e2823f6d6579e7d2bd52b7d095f9a3895672beb
Junaid-Ahmed-Sifat <junaid.ahmed.sifat@g.bracu.ac.bd>
2024-11-05 02:34:32 +0600

    2nd commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -26,3 +26,38 @@ function continueGame(){
     currentAlphabetElement.innerText = alphabet;
     setBackGroundColorById(alphabet);
 }
+
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log(" presser pressed: ",playerPressed)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if (playerPressed===expectedAlphabet){
+
+        const currentScoreElement= document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        // increase +1
+        const newScore = currentScore+1;
+        currentScoreElement.innerText=newScore;
+
+
+        RemoveBackGroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        const currentLifeElement =document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife-1;
+        currentLifeElement.innerText=newLife;
+    }
+
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress)
--- scripts/utility.js
+++ scripts/utility.js
@@ -23,5 +23,9 @@ function setBackGroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add("bg-orange-400");
 
+}
+function RemoveBackGroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove("bg-orange-400");
 
 }
