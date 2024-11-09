59d4c31e4e2f94d09bc5ed0308371ca5091ec37a
Junaid-Ahmed-Sifat <junaid.ahmed.sifat@g.bracu.ac.bd>
2024-11-10 01:05:58 +0600

    3rd  commit

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -9,11 +9,6 @@
 
 
 
-function play(){
-    hideElementById('home-screen');
-    showElementById('play-ground');
-    continueGame();
-}
 
 
 
@@ -31,33 +26,89 @@ function handleKeyboardButtonPress(event){
     const playerPressed = event.key;
     console.log(" presser pressed: ",playerPressed)
 
+    if (playerPressed=="Escape"){
+        gameOver()
+    }
+
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
 
     if (playerPressed===expectedAlphabet){
 
-        const currentScoreElement= document.getElementById('current-score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
-
-        // increase +1
-        const newScore = currentScore+1;
-        currentScoreElement.innerText=newScore;
-
+        // const currentScoreElement= document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        const currentScore = getTextElementValueById('current-score');
+        const UpdatedScore =currentScore+1;
+        setTextElementValueById('current-score',UpdatedScore);
+        // // increase +1
+        // newScore = currentScore+1;
+        // currentScoreElement.innerText=newScore;
+        
 
         RemoveBackGroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
-        const currentLifeElement =document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
+        // const currentLifeElement =document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
 
-        const newLife = currentLife-1;
-        currentLifeElement.innerText=newLife;
+        // const newLife = currentLife-1;
+        // currentLifeElement.innerText=newLife;
+        const currentLife=getTextElementValueById('current-life');
+        const UpdatedLife =currentLife-1;
+        setTextElementValueById('current-life',UpdatedLife);
+
+        if (UpdatedLife===0){
+            gameOver()
+        }
+
+
+
     }
 
 }
 
 document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
+
+function play(){
+    // hide everything show only playGround
+    hideElementById('home-screen');
+    hideElementById('final-score');
+    showElementById('play-ground');
+
+
+
+    // reset score 
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
+
+    continueGame();
+
+    
+}
+
+
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById("last-score",lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    RemoveBackGroundColorById(currentAlphabet)
+    
+}
